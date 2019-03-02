25b4cb521788c99304a302c5f4653eceb2b68675
sarashin <sarashina.nikki65@gmail.com>
2019-03-02 16:58:19 +0900

    modify html

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,8 @@
 class BooksController < ApplicationController
   def latest
+    dates1 = '%2019/03%'
+    dates2 = '%2019/3%'
+    @book_infos = Book.where("publish_date LIKE ?", dates1).or(Book.where("publish_date LIKE ?", dates2))
   end
 
   def archives
