f374fc088e452c7a71c1a3862a5b8982333bef65
sarashin <sarashina.nikki65@gmail.com>
2019-03-03 02:49:43 +0900

    add archive func

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,10 +3,14 @@ require 'time'
 class BooksController < ApplicationController
   def latest
     t = Time.new
-    dates1 = "%"+t.strftime("%Y/%m")+"%"
-    @book_infos = Book.where("publish_date LIKE ?", dates1)
+    dates = "%"+t.strftime("%Y/%m")+"%"
+    @book_infos = Book.where("publish_date LIKE ?", dates)
   end
 
   def archives
+    t = Time.parse(params[:publish_date])
+    dates = "%"+t.strftime("%Y/%m")+"%"
+    @year_month = "#{t.year}年#{t.month}月"
+    @book_infos = Book.where("publish_date LIKE ?", dates)
   end
 end
